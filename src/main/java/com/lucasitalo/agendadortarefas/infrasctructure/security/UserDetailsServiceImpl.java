d2009a41bfeb3d806cc5aa82dfe0e175b9123886
Lucas Ítalo <lucaasitaloo@gmail.com>
2025-11-07 10:49:46 -0300

    adicionadas configurações de segurança

---
+++ src/main/java/com/lucasitalo/agendadortarefas/infrasctructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.lucasitalo.agendadortarefas.infrasctructure.security;
+
+import com.lucasitalo.agendadortarefas.business.dto.UsuarioDTO;
+import com.lucasitalo.agendadortarefas.infrasctructure.client.UsuarioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+
+    @Autowired
+    private UsuarioClient client;
+
+
+    public UserDetails carregaDadosUsuario(String email, String token){
+
+        UsuarioDTO usuarioDTO = client.buscaUsuarioPorEmail(email, token);
+        return User
+                .withUsername(usuarioDTO.getEmail())
+                .password(usuarioDTO.getSenha())
+                .build();
+    }
+}
